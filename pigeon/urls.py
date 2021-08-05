1509ea823d784b13de50bbd46d5ac988cebb6343
Yatin T <yatint5@gmail.com>
2021-08-05 19:44:49 +0530

    fixup! Update user backend

--- pigeon/urls.py
+++ pigeon/urls.py
@@ -1,13 +1,16 @@
 # url endpoints for the admin, frontend and backend
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from frontend.views import index
 
-# MAIN app urls.py
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat/', include('frontend.urls')),
     path("chat/", include('backend.chat.urls')),
     path('api/accounts/', include('backend.chat.urls')),
-    path('', include('backend.users.urls'))
+    path('', include('backend.users.urls')),
 
+    # for all urls react router
+    re_path(r'^.*/', index)
 ]
