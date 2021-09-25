05254f48fb996f174f8cd0ad3fb72c94b97f0b95
Yatin T <yatint5@gmail.com>
2021-09-26 01:25:03 +0530

    Add ws for messages

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -5,7 +5,7 @@ from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth import get_user_model
 from django.contrib.sessions.models import Session
-from .models import ChatGroup, Invitation
+from .models import ChatGroup, Invitation, Message
 
 User = get_user_model()
 
@@ -13,7 +13,7 @@ User = get_user_model()
 @database_sync_to_async
 def get_user_email(session_id):
     """
-        Gets the user's email from a given 
+        Gets the user's email from a given
         session_id
     """
     value = None
@@ -51,7 +51,7 @@ def check_exists_and_authenticated(user_email):
 @database_sync_to_async
 def update_private_channel_layer_value(user_email, value):
     """
-        Sets the private_channel_layer field in 
+        Sets the private_channel_layer field in
         the user model to the channel's name
     """
     user = User.objects.get(email=user_email)
@@ -62,7 +62,7 @@ def update_private_channel_layer_value(user_email, value):
 @database_sync_to_async
 def send_group_names(user_email):
     """
-        Takes the user's email as input and 
+        Takes the user's email as input and
         returns their chat group names
     """
     group_names = []
@@ -72,6 +72,62 @@ def send_group_names(user_email):
         chat_room_name = ChatGroup.objects.get(id=chat_room_id).group_name
         group_names.append((str(chat_room_id), chat_room_name))
     return group_names
+
+
+def get_session_id(value):
+    """
+        Convert the value to string.
+        Extract the session id and return it
+    """
+    string = value.decode('utf-8')
+    regex = r'sessionid=([\w]{32})'
+    return re.findall(regex, string)[0]
+
+
+async def get_user_email_and_session_data(scope):
+    """
+        Extract the session id & user_email and 
+        return them and whether access should be
+        granted or not
+    """
+    output = {'access': False}
+    user_email = None
+    session_id = None
+
+    try:
+        headers = scope['headers']
+    except KeyError:
+        return output
+
+    for (key, value) in headers:
+        if key == b"cookie":
+            try:
+                session_id = get_session_id(value)
+                user_email = await get_user_email(session_id)
+            except Exception as e:
+                return output
+
+    if await check_exists_and_authenticated(user_email):
+        output['access'] = True
+        output['user_email'] = user_email
+        output['session_id'] = session_id
+
+    return output
+
+
+@database_sync_to_async
+def send_group_messages(chat_id):
+    """
+        Returns all messages on a given chat id
+    """
+    messages = []
+    queryset = Message.objects.filter(
+        chat_room__id=chat_id)
+
+    for value in queryset:
+        messages.append([value.owner.username, value.owner.email, value.text])
+
+    return messages
 
 
 class PrivateChannelConsumer(AsyncWebsocketConsumer):
@@ -87,27 +143,13 @@ class PrivateChannelConsumer(AsyncWebsocketConsumer):
         'private_channel_layer' field, accepts the connection and
         sends them their chat group list.
         """
-        self.user_email = None
 
-        try:
-            headers = self.scope['headers']
-        except KeyError:
-            await self.close()
+        output = await get_user_email_and_session_data(self.scope)
 
-        for (key, value) in headers:
-            if key == b"cookie":
-                string = value.decode('utf-8')  # convert bytes to string
-                self.session_id = re.findall(
-                    r'sessionid=([\w]{32})',
-                    string
-                )[0]  # get the session id
-                try:
-                    self.user_email = await get_user_email(self.session_id)
-                except Exception as e:
-                    await self.close()
+        if output['access']:
+            self.user_email = output['user_email']
+            self.session_id = output['session_id']
 
-        all_ok = await check_exists_and_authenticated(self.user_email)
-        if all_ok:
             self.room_group_name = "group-list-for-" + \
                 filter_email(self.user_email)
 
@@ -146,3 +188,50 @@ class PrivateChannelConsumer(AsyncWebsocketConsumer):
         if event['email'] == self.user_email:
             group_names = await send_group_names(self.user_email)
             await self.send(json.dumps(group_names))
+
+    async def send_messages(self, event):
+        """
+            Sends the requested chat room's messages
+        """
+        print('send messages called')
+        messages = await send_group_messages(event['chat_id'])
+        await self.send(json.dumps(messages))
+
+
+@database_sync_to_async
+def get_private_layer_name(user_email):
+    return User.objects.get(email=user_email).private_channel_layer
+
+
+class GroupChannelConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        """
+        """
+        output = await get_user_email_and_session_data(self.scope)
+        if output['access']:
+            self.user_email = output['user_email']
+            self.session_id = output['session_id']
+
+            self.chat_id = self.scope['url_route']['kwargs']['group_id']
+
+            await self.channel_layer.group_add(
+                self.chat_id,
+                self.channel_name
+            )
+
+            await self.accept()
+
+            self.room_group_name = await get_private_layer_name(self.user_email)
+            await self.send(self.room_group_name)
+
+            # send group's messages on the private layer
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    "type": "send_messages",  # name of handler to the called
+                    "chat_id": self.chat_id
+                }
+            )
+
+        else:
+            await self.close()
--- backend/chat/routing.py
+++ backend/chat/routing.py
@@ -1,8 +1,9 @@
 # chat/routing.py
 from django.urls import re_path
-from .consumers import PrivateChannelConsumer  # ,  ChatConsumer
+from .consumers import PrivateChannelConsumer, GroupChannelConsumer
 
 websocket_urlpatterns = [
+    re_path(r'^chat/(?P<group_id>[\w-]{36})/$',
+            GroupChannelConsumer.as_asgi()),
     re_path(r'^chat/', PrivateChannelConsumer.as_asgi()),
-    # re_path(r'^chat/(?P<group_id>[\w-]{36})/$', ChatConsumer.as_asgi()),
 ]
