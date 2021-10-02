7a72b56391363e0b70f98b7b3aeeaf384ca470b1
Yatin T <yatint5@gmail.com>
2021-10-03 00:38:19 +0530

    Add files

--- pigeon/asgi.py
+++ pigeon/asgi.py
@@ -1,13 +1,9 @@
 # the asgi configuration file is used to configure ASGI settings of the
 # app
-from channels.security.websocket import AllowedHostsOriginValidator
 from channels.middleware import BaseMiddleware
-from channels.db import database_sync_to_async
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.contrib.sessions.models import Session
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
 import backend.chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pigeon.settings')
