95f4964bf0ea86ac5a652e938479ad2b784b24b5
Yatin T <yatint5@gmail.com>
2021-10-05 13:22:38 +0530

    Add django.setup() in asgi.py

--- pigeon/asgi.py
+++ pigeon/asgi.py
@@ -1,16 +1,14 @@
 # the asgi configuration file is used to configure ASGI settings of the
 # app
-from channels.security.websocket import AllowedHostsOriginValidator
-from channels.middleware import BaseMiddleware
-from channels.db import database_sync_to_async
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.contrib.sessions.models import Session
-from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
+import django
 import backend.chat.routing
+from channels.middleware import BaseMiddleware
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pigeon.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),  # for http protocol we are using asgi application
