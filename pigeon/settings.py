179367dea02546434d91eabb2c99e4bcfb82b360
Yatin T <yatint5@gmail.com>
2021-10-05 20:33:10 +0530

    Remove static files

--- pigeon/settings.py
+++ pigeon/settings.py
@@ -146,5 +146,5 @@ CSRF_COOKIE_HTTPONLY = True
 SESSION_COOKIE_HTTPONLY = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR/'frontend'/'static')]
