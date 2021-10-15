68bf45d568baf71a4c30027d3582779ef91afa91
Yatin T <yatint5@gmail.com>
2021-10-15 07:16:21 +0530

    Allow ws on localhost and pigeon-chat-application

--- pigeon/asgi.py
+++ pigeon/asgi.py
@@ -8,13 +8,16 @@ import os
 import backend.chat.routing
 from channels.middleware import BaseMiddleware
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import OriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pigeon.settings')
 
+
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),  # for http protocol we are using asgi application
-    "websocket": BaseMiddleware(
+    "websocket": OriginValidator( BaseMiddleware(
         URLRouter(backend.chat.routing.websocket_urlpatterns)
-    )
+    ),["http://pigeon-chat-application.herokuapp.com","http://localhost"])
 })
