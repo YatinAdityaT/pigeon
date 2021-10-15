2c4b73a7036214fa65c808334e9de183e22a9cf6
Yatin T <yatint5@gmail.com>
2021-10-16 05:07:32 +0530

    Allow all in origin validator

--- pigeon/asgi.py
+++ pigeon/asgi.py
@@ -1,23 +1,22 @@
 # the asgi configuration file is used to configure ASGI settings of the
 # app
-
 import django
 django.setup()  # WARNING: make sure that this is on top of this file.
 
-import os
-import backend.chat.routing
-from channels.middleware import BaseMiddleware
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import OriginValidator
+
 from django.core.asgi import get_asgi_application
+from channels.security.websocket import OriginValidator
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.middleware import BaseMiddleware
+import backend.chat.routing
+import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pigeon.settings')
 
 
-
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),  # for http protocol we are using asgi application
-    "websocket": OriginValidator( BaseMiddleware(
+    "websocket": OriginValidator(BaseMiddleware(
         URLRouter(backend.chat.routing.websocket_urlpatterns)
-    ),["http://pigeon-chat-application.herokuapp.com","http://localhost"])
+    ), ["*", "http://pigeon-chat-application.herokuapp.com", "http://localhost:8000"])
 })
